3e6035459bb17b296d349aa72adfae53bcbecf9b
Ian Hutcheson <penelope15@gmail.com>
2018-08-19 08:06:01 -0700

    Add title comments and skeleton code

---
+++ Singly Linked Lists.c
@@ -0,0 +1,21 @@
+// Filename: Singly Linked Lists.c
+//
+// These are notes and sandbox code for the CS50 2018 Week 4 short video
+// entitled "Singly Linked Lists".
+//
+// Date: 08/19/2018
+//
+// Name: Ian Hutcheson
+//
+// IDE: Microsoft Visual Studio 2017
+
+
+
+#include <stdio.h>
+
+int main(void) {
+
+
+
+	return;
+}
